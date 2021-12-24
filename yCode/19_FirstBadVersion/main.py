75c62ede2e3b629da46ba1460593e1f8c150dfdb
Yuvi <yuvarajselva@hotmail.com>
2021-12-24 18:43:16 +0530

    Added first bad version

---
+++ yCode/19_FirstBadVersion/main.py
@@ -0,0 +1,32 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+
+        while left < right:
+            mid = left + (right - left) // 2
+            print(mid)
+
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid+1
+
+        return left
+"""
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version.
+"""
